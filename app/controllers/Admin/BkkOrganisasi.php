b460b630687041e2db3cdf7d9b38ab09b7968a4f
benita alexandra <124318206+menayangbenita@users.noreply.github.com>
2024-04-04 21:07:32 +0700

    BISA ANJAYY

--- app/controllers/Admin/BkkOrganisasi.php
+++ app/controllers/Admin/BkkOrganisasi.php
@@ -2,6 +2,8 @@
 
 class BkkOrganisasi extends Controller
 {
+    public $model_name = "Bkk";
+
     public function index()
     {
         $data['judul'] = 'Struktur Organisasi BKK';
@@ -10,5 +12,18 @@ class BkkOrganisasi extends Controller
         $this->view('templates-admin/header', $data);
         $this->view('admin/bkk/strukturOrganisasi', $data);
         $this->view('templates-admin/footer');
+    }
+
+    public function tambah()
+    {
+        if ($this->model("$this->model_name", 'Organisasi_model')->tambahData($_POST) > 0) {
+            Flasher::setFlash('BERHASIL', 'Ditambah', 'success');
+            header('Location: ' . BASEURL . '/BkkOrganisasi');
+            exit;
+        } else {
+            Flasher::setFlash('GAGAL', 'Ditambah', 'danger');
+            header('Location: ' . BASEURL . '/BkkOrganisasi');
+            exit;
+        }
     }
 }
